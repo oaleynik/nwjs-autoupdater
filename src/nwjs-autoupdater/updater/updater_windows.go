2fa6e97c3859b5fcc55a7407a7a04bfaf0e539a7
Oleg Aleynik <oleg.aleynik@gmail.com>
2017-06-14 18:40:20 +0300

    fix: use custom zip extraction method
    mholt/archiver package can't currently handle symbolic links
    
    This adds custom zip extraction based on
    https://github.com/mholt/archiver/issues/21#issuecomment-303991010
    
    Closes #8

--- src/nwjs-autoupdater/updater/updater_windows.go
+++ src/nwjs-autoupdater/updater/updater_windows.go
@@ -3,14 +3,14 @@ package updater
 import (
 	"path/filepath"
 
-	"github.com/mholt/archiver"
+	"nwjs-autoupdater/unzip"
 )
 
 func Update(bundle, instDir, appName string) (error, string) {
 	appExecName := appName + ".exe"
   appExec := filepath.Join(instDir, appExecName)
 
-  err := archiver.Zip.Open(bundle, instDir)
+  err := unzip.Unzip(bundle, instDir)
 	if err != nil {
 		return err, appExec
 	}
